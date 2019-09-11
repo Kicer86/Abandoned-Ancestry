5f69be518799bd2ec8277c257409663e5b7d3a9e
Michał Walenciak <Kicer86@gmail.com>
2019-09-11 19:24:50 +0200

    we need more shakin stevens

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Abondoned Ancestry");
     BasicObject shakinStevens(20, 20, 100, 50);
+    BasicObject shakinStevens2(200, 200, 100, 50);
     BasicDrawable instance;
 
     while (window.isOpen())
@@ -22,9 +23,13 @@ int main() {
         window.clear(sf::Color(210, 200, 222));
 
         instance.draw(&shakinStevens, &window);
+        instance.draw(&shakinStevens2, &window);
 
         shakinStevens.moveTo(sf::Vector2f(20 + (rand() % 10) - 5,
                                    20 + (rand() % 10) - 5));
+
+        shakinStevens2.moveTo(sf::Vector2f(200 + (rand() % 10) - 5,
+                                           200 + (rand() % 10) - 5));
 
         window.display();
     }
